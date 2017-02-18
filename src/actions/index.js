3ff8a127e21a997e3227d0a02f1bcc342a529c81
Dan Le <dhl1337@gmail.com>
2017-02-17 23:37:56 -0700

    navigate delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,7 +36,7 @@ export function fetchPost(id) {
 }
 
 export function deletePost(id) {
-    const request = axios.delete(`${ROOT_URL}/POSTS/${id}${API_KEY}`);
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
     return {
         type: DELETE_POST,
